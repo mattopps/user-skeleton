2ba58b5a685a50323d10a7d8896c5e0c7b6910e0
Matthew Oppenheimer <mattopps@gmail.com>
2016-03-15 13:58:28 -0400

    add basic user skeleton. edit css

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,29 @@
+helpers do
+  def logged_in?
+    !!current_user
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  # def authorize_access(object)
+  #   halt 403, "you are not authorized to perform this operation" unless logged_in? && current_user.id == object.user_id
+  # end
+
+  def owned?(object)
+    logged_in? && current_user.id == object.user_id
+  end
+
+   def authorize_access(object)
+    halt 403, "you are not authorized to perform this operation" unless owned?(object)
+  end
+
+  def proved_self
+    logged_in? && session[:user_id] == @user.id
+  end
+
+  def prevent_tampering
+    halt 403, "you are not authorized to perform this operation" unless proved_self
+  end
+end
---
+++ public/js/application.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+//  $('#edit-post-link').on("click", function(e){
+//    e.preventDefault();
+// $("update-post-form-container").show()
+// $("#post-details").hide();
+//  });
+
+
+
+
+});
